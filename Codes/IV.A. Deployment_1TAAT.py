a69f463ceb2866c9a6c6088eb03fefab8c1d03aa
Seb943 <55142014+Seb943@users.noreply.github.com>
2021-01-03 19:57:21 +0100

    Update IV.A. Deployment_1TAAT.py

--- Codes/IV.A. Deployment_1TAAT.py
+++ Codes/IV.A. Deployment_1TAAT.py
@@ -57,6 +57,8 @@ try:
             df = pd.DataFrame(raw).iloc[1:] # First row may contain useless data
             while (time() - df['date'].iloc[-1]) > period : #Check we've got the very recent candle : we stay here until Poloniex delivers it
                 print('Waiting for actualized data...');sleep(1)
+                raw = polo.returnChartData(pair, period = period, start = int(time()) - period*1000)
+                df = pd.DataFrame(raw).iloc[1:] # First row may contain useless data
             df['date'] = pd.to_datetime(df["date"], unit='s')
             df = df[['close', 'date', 'high', 'low', 'open', 'volume']] # only keep required data
 
